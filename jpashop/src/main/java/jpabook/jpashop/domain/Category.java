a99fac8beb556796a276e19dfe4e8d727069cf4b
umingming <u_0112@naver.com>
2022-03-17 16:01:26 +0900

    Add manyToMany annotation

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -7,6 +7,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
@@ -21,6 +26,16 @@ public class Category {
 	
 	private String name;
 	
+	@ManyToMany
+	@JoinTable(name = "category_item",
+		joinColumns = @JoinColumn(name = "category_id"),
+		inverseJoinColumns = @JoinColumn(name = "item_id"))
 	private List<Item> items = new ArrayList<>();
 	
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+	
+	@OneToMany(mappedBy = "parent")
+	private List<Category> child = new ArrayList<>();
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
@@ -7,7 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +30,7 @@ public class Item {
 	private String name;
 	private int price;
 	private int stockQuantity;
+	
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categoryies = new ArrayList<>();
 }
