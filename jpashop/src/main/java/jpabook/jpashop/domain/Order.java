46ce9ff33e2166f431707fb116ab96636482f080
umingming <u_0112@naver.com>
2022-03-17 16:11:37 +0900

    Update OrderStatus to enum
    *왜 일반 class면 에러가 날까?

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -39,5 +41,6 @@ public class Order {
 	
 	private LocalDateTime orderDateTime;
 	
+	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderStatus.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderStatus.java
@@ -1,5 +1,5 @@
 package jpabook.jpashop.domain;
 
-public class OrderStatus {
-
+public enum OrderStatus {
+	
 }
